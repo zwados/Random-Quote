90cfd999bd9317c7cd1c10da57d412f758174aa1
Łukasz Rozwadowski <lukasz.rozwadowski7@gmail.com>
2017-11-27 21:57:25 +0100

    second commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+$.ajaxSetup({ cache: false });
+
 var tweetLink = 'https://twitter.com/intent/tweet?text=';
 var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
 
